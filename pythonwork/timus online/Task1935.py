e271fb49f66ed6aa2f36453ec31d63930fe41522
FNGApex <dr.gorodnov@gmail.com>
2022-01-14 20:20:08 -0800

    Update Task1935.py

--- pythonwork/timus online/Task1935.py
+++ pythonwork/timus online/Task1935.py
@@ -2,3 +2,14 @@
 n = int(input())
 wingsAmount = list(map(int, input().split()))
 print(sum(wingsAmount)+max(wingsAmount))
+
+#   a n b
+#   left of n = max(a,n)
+#   right of n = max(b,n)
+#   a+b < n+max(a,b)
+#   n,a,b n+max(a,b) 
+#   
+#   n = min
+#   a > n
+#   b > n
+#   
