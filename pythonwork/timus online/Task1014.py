d977a83c01e394f15b5f6afccd1ef803dbe980e6
FNGApex <dr.gorodnov@gmail.com>
2022-01-14 18:44:23 -0800

    added python work
    added a folder to store all of my python applets as well as the partial solution to task 1014 on timus.online

---
+++ pythonwork/timus online/Task1014.py
@@ -0,0 +1,40 @@
+from math import sqrt, floor
+n = int(input())
+if n == 1:
+  print(1)
+else:
+  factors = []
+  run = True
+  ans = -1
+  while (run):
+    rootn = floor(sqrt(n))
+    for i in range(9,1,-1):
+      if n % i == 0: 
+        factors.append(i)
+        n = n//i
+        ans = 1
+        if i > 9: 
+          ans = -1
+          run = False
+        break
+    else:
+      break
+
+  # Last Factor Check
+  if n > 9:
+    ans = -1
+  elif n == 1:
+    n = 0
+  else:
+    factors.append(n)
+
+  #Sorting Answers
+  if ans == -1:
+    print(ans)
+  else:
+    ans = ""
+    factors = sorted(factors)
+    for i in factors:
+      ans += str(i)
+    print(ans)
+  #
