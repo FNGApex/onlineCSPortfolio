7dc59e0ca8e61e7f2cae9aab27e2b9377fd584fb
FNGApex <dr.gorodnov@gmail.com>
2022-01-19 19:27:30 -0800

    Styling Editing to scale with resolution
    editing text font and use of EM units to fit to all resolutions at the top bar

---
+++ website/index.js
@@ -0,0 +1,9 @@
+resolution = {
+  height: window.innerHeight,
+  width: window.innerWidth
+}
+scale = {
+  height:resolution.height/1080,
+  width:resolution.width/1920
+}
+document.documentElement.style.setProperty("--resolutionBasedFontSize", "10%");
