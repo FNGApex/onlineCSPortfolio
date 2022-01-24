b38e3ab2082adb446e9f85d363a9841df2901436
Apex <dr.gorodnov@gmail.com>
2022-01-24 09:12:39 -0800

    styling updates

--- website/index.js
+++ website/index.js
@@ -1,31 +1,30 @@
 let scale = {
   height: window.innerHeight / 1080,
   width: window.innerWidth / 1920,
-  update: function(){
+  update: function () {
     scale.height = window.innerHeight / 1080;
     scale.width = window.innerWidth / 1920;
   }
 }
+
 function scalebar() {
   scale.update();
   console.log("Hi");
-  document.documentElement.style.setProperty("--resolutionBasedFontSize", (130 * scale.width).toString() + "%");
+  document.documentElement.style.setProperty("--scale", (130 * scale.width).toString() + "%");
 };
 scalebar();
 window.onresize = scalebar
 let pageContent = document.getElementById("pageContent");
-document.getElementById("homeButton").addEventListener("click",function(e){
-  pageContent.innerHTML = "<img src='https://cheaphostings.net/html/tf/nick-berg/nick-berg/assets/img/me.png'>";
-  pageContent.innerHTML += "<div><h1> Hello </h1></div>";
-  pageContent.style.setProperty("display","flex");
-},false);
-document.getElementById("personalButton").addEventListener("click",function(e){
-  pageContent.innerHTML = "Personal";
-},false);
-document.getElementById("projectsButton").addEventListener("click",function(e){
-  pageContent.innerHTML = "Projects";
-},false);
-document.getElementById("contactButton").addEventListener("click",function(e){
-  pageContent.innerHTML = "<h1> Contact me: </h1>";
-  pageContent.innerHTML += "<div id='contactmeform'> Buisness Inqueries:<a href='mailto:dr.stroev@gmail.com?subject=Buisness%20Inqueries'>dr.stroev@gmail.com</a> </div>";
-},false);  
+let homeButtonClick = function (e) {
+  pageContent.style.setProperty("display", "flex");
+}
+let personalButtonClick = function (e) {
+}
+let projectsButtonClick = function (e) {
+}
+let contactButtonClick = function (e) {
+}
+document.getElementById("homeButton").addEventListener("click", homeButtonClick, false);
+document.getElementById("personalButton").addEventListener("click", personalButtonClick, false);
+document.getElementById("projectsButton").addEventListener("click", projectsButtonClick, false);
+document.getElementById("contactButton").addEventListener("click", contactButtonClick, false);
--- website/startserver.bat
+++ website/startserver.bat
@@ -1,2 +1,3 @@
 echo Starting Python Server
+cd C:\Users\drgor\Desktop\onlineCSPortfolio\website\
 python -m http.server 7600
