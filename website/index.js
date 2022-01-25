fc48afb8a4e0971eef56e30ebb9c2e53c0405350
Apex <dr.gorodnov@gmail.com>
2022-01-25 12:28:40 -0800

    topheader is now sticky

--- website/index.js
+++ website/index.js
@@ -8,7 +8,9 @@ let scale = {
 }
 let lastKnownScrollPosition = 0;
 let ticking = false;
-let mainHeaderHeight = document.getElementById("mainHeader").style.getPropertyValue("height");
+let mainHeader = document.getElementById("mainHeader");
+let mainHeaderHeight = mainHeader.offsetHeight;
+
 function scalebar() {
   scale.update();
   console.log("Hi");
@@ -33,28 +35,3 @@ document.getElementById("homeButton").addEventListener("click", homeButtonClick,
 document.getElementById("personalButton").addEventListener("click", personalButtonClick, false);
 document.getElementById("projectsButton").addEventListener("click", projectsButtonClick, false);
 document.getElementById("contactButton").addEventListener("click", contactButtonClick, false);
-
-function addPermaBar(){
-  document.getElementById("mainHeader").style.maxHeight = "0px";
-  document.getElementById("mainHeader").style.maxWidth = "0px";
-  console.log("Hello");
-}
-function removePermaBar(){
-  document.getElementById("mainHeader").style.maxHeight = "auto";
-  document.getElementById("mainHeader").style.maxWidth = "auto";
-  console.log("Goodbye");
-}
-document.addEventListener("scroll", function (e) {
-  lastKnownScrollPosition = window.scrollY;
-
-  if (!ticking) {
-    if (lastKnownScrollPosition >= mainHeaderHeight) {
-      window.requestAnimationFrame(addPermaBar);
-      console.log(lastKnownScrollPosition);
-      console.log(window.scrollY);
-      console.log(document.getElementById("mainHeader").style.height);
-    } else {
-      window.requestAnimationFrame(removePermaBar);
-    }
-  }
-});
