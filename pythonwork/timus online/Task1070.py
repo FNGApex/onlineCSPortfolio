d5358b038cc64d77dbe0345bf8b106aa54c91053
FNGApex <dr.gorodnov@gmail.com>
2022-05-11 21:05:36 -0700

    Update Task1070.py

--- pythonwork/timus online/Task1070.py
+++ pythonwork/timus online/Task1070.py
@@ -1,3 +1,12 @@
+def determine_flight_time(starttime: list, endtime: list, time_diffrence: int):
+    start_hour = starttime[0]
+    end_hour = endtime[0]
+    if start_hour > end_hour+time_diffrence:
+        return 24+end_hour-start_hour+time_diffrence
+    else:
+        return end_hour-start_hour+time_diffrence
+
+
 flight_out_start, flight_out_end = [
     list(map(int, i.split("."))) for i in list(input().split())]
 flight_back_start, flight_back_end = [
@@ -8,25 +17,14 @@ max_time_diffrence = 5
 flight_out_additive = 0
 flight_back_additive = 0
 time_diffrence = 0
-if flight_out_start[0] > flight_out_end[0] or (flight_out_start[0] == flight_out_end[0] and flight_out_start[1] > flight_out_end[1]):
-    flight_out_additive = 24
-    #print("Additive True")
-if (flight_back_start[0] > flight_back_end[0]) or (flight_back_start[0] == flight_back_end[0] and flight_back_start[1] > flight_back_end[1]):
-    flight_back_additive = 24
-for proposed_time_diffrence in range(5, -1, -1):
-    #print("Proposed_Time_Diffrence =", proposed_time_diffrence)
-
-    flight_time_there = flight_out_end[0] - flight_out_start[0] + \
-        proposed_time_diffrence + flight_out_additive
-
-    #print("Flight_Time_There =", flight_time_there)
-
-    flight_time_back = flight_back_end[0] - flight_back_start[0] - \
-        proposed_time_diffrence + flight_back_additive
-
+for proposed_time_diffrence in range(-5, 6):
+    flight_time_there = determine_flight_time(
+        flight_out_start, flight_out_end, proposed_time_diffrence)
+    flight_time_back = determine_flight_time(
+        flight_back_start, flight_back_end, proposed_time_diffrence*-1)
     #print("Flight_Time_Back =", flight_time_back)
 
     if flight_time_there == flight_time_back and flight_time_there <= 6:
         time_diffrence = proposed_time_diffrence
         #print("Found Match")
-print(time_diffrence)
+print(abs(time_diffrence))
