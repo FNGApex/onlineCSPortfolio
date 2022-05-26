5598e758f1bb39a337284aa52ff19487c05b0979
FNGApex <dr.gorodnov@gmail.com>
2022-05-25 19:48:14 -0700

    Update Task1070.py

--- pythonwork/timus online/Task1070.py
+++ pythonwork/timus online/Task1070.py
@@ -1,8 +1,15 @@
-def determine_flight_time(start_time: list, end_time: list, current_time_diffrence: int):
-    if start_time > end_time+current_time_diffrence*60:
-        return 1440+end_time-start_time+current_time_diffrence*60
+def determine_flight_time(start_time: int, end_time: int, current_time_diffrence: int):
+    if start_time > end_time + (current_time_diffrence*60):
+        return 1440+end_time - start_time + (current_time_diffrence*60)
+    elif end_time-start_time+(current_time_diffrence*60) > 1440:
+        return end_time-(start_time+1440-(current_time_diffrence*60))
     else:
-        return end_time-start_time+current_time_diffrence*60
+        return end_time-start_time + (current_time_diffrence*60)
+
+
+def crude_to_polished_time(time_str: list):
+    return int(time_str.split(
+        ".")[0]) * 60+int(time_str.split(".")[1])
 
 
 max_flight_time = 360
@@ -10,15 +17,11 @@ time_diffrence = -1
 
 flight_out_start, flight_out_end = 0, 0
 current_flight_crude = input().split()
-flight_out_start = int(current_flight_crude[0].split(
-    ".")[0]) * 60+int(current_flight_crude[0].split(".")[1])
-flight_out_end = int(current_flight_crude[1].split(
-    ".")[0]) * 60+int(current_flight_crude[1].split(".")[1])
+flight_out_start = crude_to_polished_time(current_flight_crude[0])
+flight_out_end = crude_to_polished_time(current_flight_crude[1])
 current_flight_crude = input().split()
-flight_back_start = int(current_flight_crude[0].split(
-    ".")[0]) * 60+int(current_flight_crude[0].split(".")[1])
-flight_back_end = int(current_flight_crude[1].split(
-    ".")[0]) * 60+int(current_flight_crude[1].split(".")[1])
+flight_back_start = crude_to_polished_time(current_flight_crude[0])
+flight_back_end = crude_to_polished_time(current_flight_crude[1])
 
 #
 # print(flight_out_start, flight_out_end,
@@ -30,9 +33,8 @@ for proposed_time_diffrence in range(-5, 6):
         flight_out_start, flight_out_end, proposed_time_diffrence)
     flight_time_back = determine_flight_time(
         flight_back_start, flight_back_end, proposed_time_diffrence*-1)
-    #print("Flight_Time_Back =", flight_time_back)
 
-    if abs(flight_time_there - flight_time_back) <= 10 and flight_time_there <= max_flight_time:
+    if abs(flight_time_there - flight_time_back) <= 10 and flight_time_there <= max_flight_time and flight_time_back <= max_flight_time:
         time_diffrence = abs(proposed_time_diffrence)
         #print("Found Match")
 print(time_diffrence)
